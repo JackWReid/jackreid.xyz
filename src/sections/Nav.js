762674a2df70be923d295b562b114009619318b6
Jack Reid <emailjackreid@gmail.com>
2017-04-01 23:03:57 +0100

    Remove last remnants of glamor ✨

--- src/sections/Nav.js
+++ src/sections/Nav.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { css } from 'glamor';
+import styled from 'styled-components';
 
-const sectionStyle = {
-  marginBottom: 32,
-};
+const Section = styled.div`
+  margin-bottom: 32px;
+  & a {
+    color: inherit;
+    display: block;
+    transition: all .3s ease-in-out;
+  }
+`;
 
-const sectionTitleStyle = {
-  margin: 0,
-  fontSize: 16,
-  fontWeight: 'normal',
-};
-
-const linkStyle = {
-  color: 'inherit',
-  display: 'block',
-  transition: 'all .3s ease-in-out',
-};
+const SectionTitle = styled.h1`
+  margin: 0;
+  font-size: 16px;
+  font-weight: normal;
+`;
 
 const activeLinkStyle = {
   color: 'white',
@@ -26,50 +25,50 @@ const activeLinkStyle = {
 
 export default () => (
   <div>
-    <div {...css(sectionStyle)}>
-      <h1 {...css(sectionTitleStyle)}>Work</h1>
-      <NavLink {...css(linkStyle)} activeStyle={activeLinkStyle} to="/work/tott">Tott</NavLink>
-      <NavLink {...css(linkStyle)} activeStyle={activeLinkStyle} to="/work/slashthread">/thread</NavLink>
-      <NavLink {...css(linkStyle)} activeStyle={activeLinkStyle} to="/work/buukks">Buukks</NavLink>
-      <NavLink {...css(linkStyle)} activeStyle={activeLinkStyle} to="/work/depop">Depop</NavLink>
-      <NavLink {...css(linkStyle)} activeStyle={activeLinkStyle} to="/work/pearshaped">PearShaped</NavLink>
-    </div>
+    <Section>
+      <SectionTitle>Work</SectionTitle>
+      <NavLink activeStyle={activeLinkStyle} to="/work/tott">Tott</NavLink>
+      <NavLink activeStyle={activeLinkStyle} to="/work/slashthread">/thread</NavLink>
+      <NavLink activeStyle={activeLinkStyle} to="/work/buukks">Buukks</NavLink>
+      <NavLink activeStyle={activeLinkStyle} to="/work/depop">Depop</NavLink>
+      <NavLink activeStyle={activeLinkStyle} to="/work/pearshaped">PearShaped</NavLink>
+    </Section>
 
-    <div {...css(sectionStyle)}>
-      <h1 {...css(sectionTitleStyle)}>Trips</h1>
-      <NavLink {...css(linkStyle)} activeStyle={activeLinkStyle} to="/trip/budapest">Budapest</NavLink>
-      <NavLink {...css(linkStyle)} activeStyle={activeLinkStyle} to="/trip/edinburgh">Edinburgh</NavLink>
-      <NavLink {...css(linkStyle)} activeStyle={activeLinkStyle} to="/trip/washingtondc">Washington D.C</NavLink>
-      <NavLink {...css(linkStyle)} activeStyle={activeLinkStyle} to="/trip/boston">Boston</NavLink>
-      <NavLink {...css(linkStyle)} activeStyle={activeLinkStyle} to="/trip/nyc">New York City</NavLink>
-      <NavLink {...css(linkStyle)} activeStyle={activeLinkStyle} to="/trip/toronto">Toronto</NavLink>
-      <NavLink {...css(linkStyle)} activeStyle={activeLinkStyle} to="/trip/chicago">Chicago</NavLink>
-      <NavLink {...css(linkStyle)} activeStyle={activeLinkStyle} to="/trip/sanfrancisco">San Francisco</NavLink>
-      <NavLink {...css(linkStyle)} activeStyle={activeLinkStyle} to="/trip/yosemite">Yosemite</NavLink>
-      <NavLink {...css(linkStyle)} activeStyle={activeLinkStyle} to="/trip/losangeles">Los Angeles</NavLink>
-      <NavLink {...css(linkStyle)} activeStyle={activeLinkStyle} to="/trip/lasvegas">Las Vegas</NavLink>
-      <NavLink {...css(linkStyle)} activeStyle={activeLinkStyle} to="/trip/grandcanyon">Grand Canyon</NavLink>
-    </div>
+    <Section>
+      <SectionTitle>Trips</SectionTitle>
+      <NavLink activeStyle={activeLinkStyle} to="/trip/budapest">Budapest</NavLink>
+      <NavLink activeStyle={activeLinkStyle} to="/trip/edinburgh">Edinburgh</NavLink>
+      <NavLink activeStyle={activeLinkStyle} to="/trip/washingtondc">Washington D.C</NavLink>
+      <NavLink activeStyle={activeLinkStyle} to="/trip/boston">Boston</NavLink>
+      <NavLink activeStyle={activeLinkStyle} to="/trip/nyc">New York City</NavLink>
+      <NavLink activeStyle={activeLinkStyle} to="/trip/toronto">Toronto</NavLink>
+      <NavLink activeStyle={activeLinkStyle} to="/trip/chicago">Chicago</NavLink>
+      <NavLink activeStyle={activeLinkStyle} to="/trip/sanfrancisco">San Francisco</NavLink>
+      <NavLink activeStyle={activeLinkStyle} to="/trip/yosemite">Yosemite</NavLink>
+      <NavLink activeStyle={activeLinkStyle} to="/trip/losangeles">Los Angeles</NavLink>
+      <NavLink activeStyle={activeLinkStyle} to="/trip/lasvegas">Las Vegas</NavLink>
+      <NavLink activeStyle={activeLinkStyle} to="/trip/grandcanyon">Grand Canyon</NavLink>
+    </Section>
 
-    <div {...css(sectionStyle)}>
-      <h1 {...css(sectionTitleStyle)}>Other Photography</h1>
-      <NavLink {...css(linkStyle)} activeStyle={activeLinkStyle} to="/photography/womens-march">Women's March 2017</NavLink>
-      <NavLink {...css(linkStyle)} activeStyle={activeLinkStyle} to="/photography/essex">Essex</NavLink>
-      <NavLink {...css(linkStyle)} activeStyle={activeLinkStyle} to="/photography/commute-east">Commute East</NavLink>
-    </div>
+    <Section>
+      <SectionTitle>Other Photography</SectionTitle>
+      <NavLink activeStyle={activeLinkStyle} to="/photography/womens-march">Women's March 2017</NavLink>
+      <NavLink activeStyle={activeLinkStyle} to="/photography/essex">Essex</NavLink>
+      <NavLink activeStyle={activeLinkStyle} to="/photography/commute-east">Commute East</NavLink>
+    </Section>
 
-    <div {...css(sectionStyle)}>
-      <h1 {...css(sectionTitleStyle)}>Writing</h1>
-      <NavLink {...css(linkStyle)} activeStyle={activeLinkStyle} to="/writing/depop-day4">Depop Does Day 4</NavLink>
-      <NavLink {...css(linkStyle)} activeStyle={activeLinkStyle} to="/writing/accessibility-thing">That Accessibility Thing</NavLink>
-      <NavLink {...css(linkStyle)} activeStyle={activeLinkStyle} to="/writing/the-jetset-ape">The Jetset Ape</NavLink>
-      <NavLink {...css(linkStyle)} activeStyle={activeLinkStyle} to="/writing/fat-internet-historian">@fat: Internet Historian</NavLink>
-      <NavLink {...css(linkStyle)} activeStyle={activeLinkStyle} to="/writing/twitters-desktop-troubles">Twitter's Desktop Troubles</NavLink>
-      <NavLink {...css(linkStyle)} activeStyle={activeLinkStyle} to="/writing/siri-make-me-a-sandwich">Siri, Make Me A Sandwich</NavLink>
-      <NavLink {...css(linkStyle)} activeStyle={activeLinkStyle} to="/writing/dish-hackathon">Dish & The Hackathon</NavLink>
-      <NavLink {...css(linkStyle)} activeStyle={activeLinkStyle} to="/writing/internet-rhizome">Is The Internet a Rhizome?</NavLink>
-      <NavLink {...css(linkStyle)} activeStyle={activeLinkStyle} to="/writing/confessions-news-addict">Confessions of a News Addict</NavLink>
-      <NavLink {...css(linkStyle)} activeStyle={activeLinkStyle} to="/writing/music-is-changing">Music Is Changing</NavLink>
-    </div>
+    <Section>
+      <SectionTitle>Writing</SectionTitle>
+      <NavLink activeStyle={activeLinkStyle} to="/writing/depop-day4">Depop Does Day 4</NavLink>
+      <NavLink activeStyle={activeLinkStyle} to="/writing/accessibility-thing">That Accessibility Thing</NavLink>
+      <NavLink activeStyle={activeLinkStyle} to="/writing/the-jetset-ape">The Jetset Ape</NavLink>
+      <NavLink activeStyle={activeLinkStyle} to="/writing/fat-internet-historian">@fat: Internet Historian</NavLink>
+      <NavLink activeStyle={activeLinkStyle} to="/writing/twitters-desktop-troubles">Twitter's Desktop Troubles</NavLink>
+      <NavLink activeStyle={activeLinkStyle} to="/writing/siri-make-me-a-sandwich">Siri, Make Me A Sandwich</NavLink>
+      <NavLink activeStyle={activeLinkStyle} to="/writing/dish-hackathon">Dish & The Hackathon</NavLink>
+      <NavLink activeStyle={activeLinkStyle} to="/writing/internet-rhizome">Is The Internet a Rhizome?</NavLink>
+      <NavLink activeStyle={activeLinkStyle} to="/writing/confessions-news-addict">Confessions of a News Addict</NavLink>
+      <NavLink activeStyle={activeLinkStyle} to="/writing/music-is-changing">Music Is Changing</NavLink>
+    </Section>
   </div>
 );
