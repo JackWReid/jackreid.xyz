a5115035c4bc17ce1c96794892f9fe751401cdb6
Jack Reid <emailjackreid@gmail.com>
2017-04-24 22:14:41 +0100

    Fix typo

--- src/sections/Intro.js
+++ src/sections/Intro.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default () => (
   <div>
-    <p styled={{marginTop: 0}}>I make stuff on the internet. I read books. I take photos. I write things. The things I write aren’t very good, but I did English at uni so I have to do it anyway.</p>
+    <p style={{marginTop: 0}}>I make stuff on the internet. I read books. I take photos. I write things. The things I write aren’t very good, but I did English at uni so I have to do it anyway.</p>
     <p>I’m hard to teach but I’m a good listener. I learn things quickly (when they actually need to be learned). I live in London. I like cities. I hate cars. I’m handy in a pub quiz.</p>
     <p>I care about politics. I studied it. I study it. I read the news. The only sports I ever care about happen on snow. Even then I forget to watch them on TV. I don’t watch live TV. I like films and music, and arguing about award shows.</p>
     <p>I like people, mostly. I like making things for people. I’ll never make my mind up about what a personal website is for.</p>
