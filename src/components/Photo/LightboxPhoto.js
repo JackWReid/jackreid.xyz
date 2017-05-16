bcc9928cf45f3046196be02558cec21e967f794c
Jack Reid <emailjackreid@gmail.com>
2017-05-17 00:18:20 +0100

    Fix photo lightbox

--- src/components/Photo/LightboxPhoto.js
+++ src/components/Photo/LightboxPhoto.js
@@ -2,6 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   position: fixed;
   top: 0; left: 0; right: 0; bottom: 0;
   background-color: black;
@@ -10,8 +13,8 @@ const Wrapper = styled.div`
 `;
 
 const Image = styled.img`
-  margin: 0 auto;
-  width: 800vw;
+  display: block;
+  max-width: 70vw;
 `;
 
 export default ({url, closeLightbox}) => (
