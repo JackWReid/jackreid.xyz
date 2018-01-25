d49dfa33736b30feb6c4480cad185711814b9afc
Jack Reid <emailjackreid@gmail.com>
2018-01-25 19:18:05 +0000

    add a reading section

--- src/components/CurrentlyReading.js
+++ src/components/CurrentlyReading.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
-import { IntroBlock } from './Blocks';
+import { FadeWaveBlock } from './Blocks';
 
-const Block = IntroBlock.extend`
-  grid-area: reading;
+const Block = FadeWaveBlock.extend`
   opacity: ${props => props.loaded ? 1 : 0};
   transition: opacity 1s ease-in-out;
+`;
+
+const LilTitle = styled.h1`
+  margin: 0;
+  font-size: 1rem;
+  text-transform: uppercase;
+  font-family: Inconsolata, monospace;
 `;
 
 export class CurrentlyReading extends Component {
@@ -15,18 +21,17 @@ export class CurrentlyReading extends Component {
   }
 
   async componentDidMount() {
-    const response = await fetch('https://jackreid-books-api.now.sh/reading');
-    const [readingData] = await response.json();
+    const response = await fetch('https://api.jackreid.xyz/reading');
+    const readingData = await response.json();
     this.setState({readingData});
   }
 
   render() {
     const { readingData } = this.state;
-    console.log(this.state)
 
     return (
       <Block loaded={!!readingData}>
-        <h1>Currently Reading</h1>
+        <LilTitle>Currently Reading</LilTitle>
         {readingData && <h2>{readingData.title}</h2>}
         {readingData && <h3>{readingData.author}</h3>}
       </Block>
