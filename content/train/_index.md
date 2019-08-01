56f42de3be3e314449358f0a38e4a6f66b2d6dc1
Jack Reid <emailjackreid@gmail.com>
2019-08-01 12:49:26 +0100

    test train times

---
+++ content/train/_index.md
@@ -0,0 +1,8 @@
+---
+title: Trains
+date: 2019-07-23T15:51:00+01:00
+draft: false
+---
+
+<p id="trains">Loading train times...</p>
+<script src="{{ "trains.js" | absURL }}"></script>
---
+++ static/js/trains.js
@@ -0,0 +1,73 @@
+const API_URL = 'http://transportapi.com/v3/uk/train';
+const API_APP_ID = process.env['TRANSPORT_API_ID'];
+const API_APP_KEY = process.env['TRANSPORT_API_KEY'];
+
+const STATION_MAP = {
+  'Euston': 'EUS',
+  'City Thameslink': 'CTK',
+};
+
+const trainBoard = ({ station_name, station_code, departures }) => `
+  <section class="train-board">
+    <h2 class="train-board">${station_name} (${station_code.toUpperCase()})</h2>
+    ${departuresList(departures)}
+  </section>
+`;
+
+const departuresList = departures => `
+  <table class="train-board__list">
+    <thead class="train-board__list-header">
+      <th>Time</th>
+      <th>Destination</th>
+      <th>Platform</th>
+      <th>Expected</th>
+    </thead>
+    <tbody>
+    ${departures.all.map(departureListItem).join('')}
+    </tbody>
+  </table>
+`;
+
+const departureListItem = ({
+  platform,
+  operator_name,
+  aimed_departure_time,
+  destination_name,
+  status,
+  expected_departure_time
+}) => `
+  <tr class="train-board__list-item ${status === 'LATE' && 'train-board__list-item--late'}">
+    <td>${aimed_departure_time}</td>
+    <td>${destination_name}</td>
+    <td>${platform}</td>
+    <td>${status === 'LATE' ? expected_departure_time : ''}</td>
+  </tr>
+`;
+
+const attachToDOM = (html, node) => {
+  node.innerHTML = html;
+}
+
+(async () => {
+  async function fetchStationData(stationCode) {
+    const url =
+      `${API_URL}/station/${stationCode}/live.json?app_id=${API_APP_ID}&app_key=${API_APP_KEY}`;
+    const data = await fetch(url).then(d => d.json());
+    return data;
+  }
+
+  async function main() {
+    const node = document.querySelector('#trains');
+    if (!node) {
+      return;
+    }
+    const station = new URLSearchParams(document.location.search).get('station') || 'CTK';
+    const data = await fetchStationData(station);
+    const html = trainBoard(data);
+
+    return attachToDOM(html, node);
+  }
+
+  main();
+})();
+
