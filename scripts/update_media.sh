bb4034b4a01bfb7bb9cc136c4e55d6dd71837dbb
Jack Reid <emailjackreid@gmail.com>
2020-01-24 16:29:54 +0000

    update script

--- scripts/update_media.sh
+++ scripts/update_media.sh
@@ -1,28 +1,39 @@
 #!/usr/bin/env bash
 
+function installed {
+  cmd=$(command -v "${1}")
+
+  [[ -n "${cmd}" ]] && [[ -f "${cmd}" ]]
+  return ${?}
+}
+
+function die {
+  >&2 echo "Fatal: ${@}"
+  exit 1
+}
+
+deps=(curl git)
+for dep in "${deps[@]}"; do
+  installed "${dep}" || die "Missing '${dep}'"
+done
+
 echo 'Starting book and film data update'
-rm -rv ./tmp;
 git checkout -f;
 git pull origin master;
 
 # Download latest data from API
-mkdir ./tmp;
-curl -L https://api.jackreid.xyz/books/reading -o ./tmp/reading.json;
-curl -L https://api.jackreid.xyz/books/toread -o ./tmp/toread.json;
-curl -L https://api.jackreid.xyz/books/read -o ./tmp/read.json;
+curl -L https://api.jackreid.xyz/books/reading -o ./data/books/reading.json;
+curl -L https://api.jackreid.xyz/books/toread -o ./data/books/toread.json;
+curl -L https://api.jackreid.xyz/books/read -o ./data/books/read.json;
 
-curl -L https://api.jackreid.xyz/films/watched -o ./tmp/watched.json;
-curl -L https://api.jackreid.xyz/films/towatch -o ./tmp/towatch.json;
-
-# Replace data with tmp
-rm ./data/books/*; rm -r ./data/films/*;
-
-mv ./tmp/reading.json ./data/books/reading.json;
-mv ./tmp/toread.json ./data/books/toread.json;
-mv ./tmp/read.json ./data/books/read.json;
-
-mv ./tmp/watched.json ./data/films/watched.json;
-mv ./tmp/towatch.json ./data/films/towatch.json;
+curl -L https://api.jackreid.xyz/films/watched -o ./data/films/watched.json;
+curl -L https://api.jackreid.xyz/films/towatch -o ./data/films/towatch.json;
 
 # Update git
-git add data && git commit -m 'update books and films' && git push origin master;
+echo "[$(date)] Committing updated media data files"
+if [ -z "$(git status --porcelain)" ]; then
+	echo "[$(date)] No changes found"
+else
+	echo "[$(date)] Changes found"
+	git add . && git commit -m "[$(date)] Updated media data files" && git push origin master;
+fi
